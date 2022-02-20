8e0b2398edb118f698db1d04ee4174e3a5f768e1
Volodymyr Tieliezhkin <57314700+Volodymyr-T@users.noreply.github.com>
2022-02-21 00:34:11 +0100

    feat: write check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let result = false;
+  let curLength = str.length;
+  while (true) {
+    for (let i = 0; i < bC.length; i++) {
+      str = str.replace(bC[i].join(''), '');
+    }
+    if (str.length === 0) {
+      result = true;
+      break;
+    }
+    if (str.length === curLength) break; 
+    curLength = str.length;
+  }
+  return result;
 }
